6136d8d603c6b47a36536cf4560a45db24fb8fa6
Cliff Oyoh Omollo <oyoocliff471@gmail.com>
2025-07-09 15:50:58 +0300

    feat(db):Add SQLite connection and auto-migration logic

---
+++ go-auth-app/internal/database/database.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"go-auth-app/internal/model"
+	"log/slog"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+// Connect initializes the database connection and runs migrations.
+func Connect(dsn string) (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	slog.Info("Running database migrations...")
+	err = db.AutoMigrate(&model.User{})
+	if err != nil {
+		return nil, err
+	}
+	slog.Info("Database migration completed.")
+
+	return db, nil
+}
